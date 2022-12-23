fe03b088b166497fe0e7846e5c8a3c0d66094a5f
Doug Hooker <DouglassHooker@gmail.com>
2022-12-23 13:12:03 -0800

    Add some better env integration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,18 @@ require('dotenv').config({
 module.exports = {
   apps : [{
     script: 'dist/index.js',
-    watch: '.'
+    watch: '.',
+    env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        WORKER_COUNT_PER_JOB:3,
+
+        // populate these in the .env.deploy file
+        DATABASE_URL: process.env.DATABASE_URL,
+        LOGGER_API_KEY: process.env.LOGGER_API_KEY,
+        LOGGER_API_ID: process.env.LOGGER_API_ID,
+        REDIS_URL: process.env.REDIS_URL,
+    }
   }],
 
   deploy : {
@@ -19,7 +30,7 @@ module.exports = {
       path : process.env.DESTINATION,
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && npx prisma generate && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'pre-setup': 'ls /'
     }
   }
 };
