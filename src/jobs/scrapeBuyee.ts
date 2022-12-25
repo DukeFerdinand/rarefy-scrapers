229c75bf76cd90a099614d01f2c40c0dfada5b40
Doug Hooker <DouglassHooker@gmail.com>
2022-12-24 16:26:11 -0800

    begin adding some real scraping for buyee

--- src/jobs/scrapeBuyee.ts
+++ src/jobs/scrapeBuyee.ts
@@ -3,7 +3,25 @@ import playwright from 'playwright-firefox'
 import {logger} from "../logger";
 import {BuyeeJob} from "../queue";
 
-const BASE_URL = "https://buyee.jp/item/search/query{{term}}"
+const BASE_URL = "https://buyee.jp/item/search/query/{{term}}"
+
+enum Currency {
+    USD = "USD",
+    EUR = "EUR",
+    GBP = "GBP",
+    JPY = "JPY",
+    BRL = "BRL",
+}
+
+interface BuyeeSearchResult {
+    title: string,
+    price: number, // Yen
+    currency: Currency,
+    link: string,
+    seller: string,
+    timeRemaining: string,
+    bids: number,
+}
 
 export const scrapeBuyee = async (terms: BuyeeJob['terms']) => {
     logger.info(`Buyee scraper: scraping ${terms.length} terms`);
@@ -20,9 +38,44 @@ export const scrapeBuyee = async (terms: BuyeeJob['terms']) => {
             url += "/category/22260"
         }
 
+        console.log(url)
+
+        url += "?translationType=1&suggest=1"
+
         await page.goto(url)
-        const items = await page.$$('.itemList')
-        logger.info(`Buyee scraper: found ${items.length} items for ${term.name}`);
+        const items = await page.$$('.itemCard')
+
+        if (items) {
+            logger.info(`Buyee scraper: found ${items.length} items for ${term.query}`);
+            const scrapedItems: BuyeeSearchResult[] = []
+            for (const item of items) {
+                const searchResult: BuyeeSearchResult = {
+                    currency: Currency.JPY,
+                } as BuyeeSearchResult
+
+                const {title, href} = await item.$eval('.itemCard__itemName > a', (el) => {
+                    return {
+                        title: el.textContent?.trim() || "",
+                        href: el.getAttribute('href') || ''
+                    }
+                })
+
+                const price = await item.$eval('.g-price', (el) => {
+                    return parseInt(el.textContent?.replace(/[^0-9]/g, "") || "0")
+                })
+
+                scrapedItems.push({
+                    ...searchResult,
+                    title,
+                    link: href,
+                    price,
+                })
+            }
+
+            console.log(scrapedItems)
+        } else {
+            logger.info(`Buyee scraper: found no items for ${term.query}`);
+        }
         await page.close()
     }
 
